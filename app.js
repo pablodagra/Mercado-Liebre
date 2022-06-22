c75a4b2fb19b87cf8068b53fba79a18f7ffd71ee
--global <--global>
2022-06-22 13:18:03 -0300

    crando rutas

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.listen(process.env.PORT || 3000);
+
+const staticFolder = path.resolve(__dirname, "./public");
+app.use(express.static(staticFolder));
+
+app.get('/',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+  });
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+  });
