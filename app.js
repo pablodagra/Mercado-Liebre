a165a3809bff49d99227e0a4e23786b28d291b4c
pablodagra <pablodagra@hotmail.com>
2022-06-23 03:26:06 -0300

    agregando Procfile

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
-app.listen(process.env.PORT || 3000);
-
 const staticFolder = path.resolve(__dirname, "./public");
 app.use(express.static(staticFolder));
+
+const PORT = process.env.PORT || 4000;
+const HOST = process.env.HOST || 'localhost';
+app.listen(PORT, ()=>{
+  console.log('server running at http://${PORT}:${HOST}/');
+});
 
 app.get('/',(req,res) => {
     res.sendFile(path.resolve(__dirname,'./views/home.html'))
