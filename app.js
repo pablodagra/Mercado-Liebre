a0d112c63d96aa381e73fad64d7722b59c7663c0
pablodagra <pablodagra@hotmail.com>
2022-06-23 13:35:42 -0300

    Corriedo el servidor

--- app.js
+++ app.js
@@ -2,24 +2,26 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const staticFolder = path.resolve(__dirname, "./public");
-app.use(express.static(staticFolder));
-
 const PORT = process.env.PORT || 4000;
 const HOST = process.env.HOST || 'localhost';
 
 
+const staticFolder = path.resolve(__dirname, "./public");
+app.use(express.static(staticFolder));
+
+
+
 app.get('/',(req,res) => {
-    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+    res.sendFile(path.resolve(__dirname,'./view/home.html'))
 });
 
 app.get("/login", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+    res.sendFile(path.resolve(__dirname, "./view/login.html"));
   });
 
 app.get("/register", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+    res.sendFile(path.resolve(__dirname, "./view/register.html"));
   });
   app.listen(PORT, ()=>{
-    console.log('server running at http://${PORT}:${HOST}/');
+    console.log(`Server running at http://${HOST}:${PORT}/`)
   });
